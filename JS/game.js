9aad06b633ed2817445dd31b00b567a2746664aa
1pump2urface <58392501+1pump2urface@users.noreply.github.com>
2020-06-08 18:11:15 +0530

    Add files via upload

---
+++ JS/game.js
@@ -0,0 +1,21 @@
+class Game{
+    constructor(){}
+    getState(){
+        var stateRef = database.ref('GameState');
+        stateRef.on("value", function(data){
+            gameState = data.val();
+        })
+    }
+    update(state){
+        database.ref('/').update({
+       GameState:state })
+    }
+    start(){
+        if (gameState == 0){
+             player = new Player();
+             form = new Form();
+             form.display();
+        }
+    }
+    
+}
